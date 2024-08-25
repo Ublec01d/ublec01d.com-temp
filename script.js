cdbcb9168b0249265e91c73f95362b97f4df2c94
Ublec01d <ublec01d@gmail.com>
2024-08-25 17:28:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -50,12 +50,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function askPassword() {
     var a = 10 + 35 + 38 + 69 + 100; // this could also be found on my website if you tried.. maybe you'll find where it's hidden with this hint.
-    var w8w = prompt("Enter the password:");
+    var w8w = prompt("Enter the code: XXX ");
 
 
     if (w8w === a.toString()) {
         window.location.href = "https://github.com/Ublec01d/website"; // target URL
     } else {
-        alert("Incorrect password. Try harder.");
+        alert("INCORRECT CODE! Think...");
     }
 }
